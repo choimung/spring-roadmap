0eee03dae5124a7c6831bde0a01445cdeb16c9c8
hdtv <hdtv@kakao.com>
2024-01-02 19:59:45 +0900

    feat : 추가

---
+++ 김영한 MVC - 1편 중/springmvc/src/main/java/hello/springmvc/basic/reuquest/RequestBodyStringController.java
@@ -0,0 +1,53 @@
+package hello.springmvc.basic.reuquest;
+
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@Slf4j
+public class RequestBodyStringController {
+
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyStringV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String message = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("message = {}",message);
+        response.getWriter().write("ok");
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+        String message = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        log.info("message = {}",message);
+        responseWriter.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+        String body = httpEntity.getBody();
+        log.info("message = {}",body);
+        return new HttpEntity<String>("ok");
+    }
+
+
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) throws IOException {
+        log.info("message = {}",messageBody);
+        return "ok";
+    }
+}
