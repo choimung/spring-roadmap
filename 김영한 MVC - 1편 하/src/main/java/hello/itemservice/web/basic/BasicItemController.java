d2fed35179602d2f976191180a8631136562a7f9
choimung <103022503+choimung@users.noreply.github.com>
2024-01-07 19:16:35 +0900

    feat : [섹션 4] BindingResult 완료

--- 김영한 MVC - 1편 하/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 김영한 MVC - 1편 하/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -15,6 +15,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -106,41 +109,52 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String addItemV5(Item item, RedirectAttributes redirectAttributes, Model model) {
-/*
-        log.info("item.open={}",item.getOpen());
-        log.info("item regions ={}", item.getRegions());
-        log.info("item.itemType={}", item.getItemType());
-*/
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes, Model model) {
+        /*V1*/
         //검증 오류 보관
-        Map<String, String> errors = new HashMap<>();
+//        Map<String, String> errors = new HashMap<>();
 
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
-            errors.put("itemName", "상품 이름은 필수입니다.");
+//            errors.put("itemName", "상품 이름은 필수입니다.");
+//            bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
+            bindingResult.addError(
+                    new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100000) {
-            errors.put("price", "가격은 1,000원에서 1,000,000까지 허용합니다.");
+//            errors.put("price", "가격은 1,000원에서 1,000,000까지 허용합니다.");
+//            bindingResult.addError(new FieldError("item", "price", "가격은 1,000원에서 1,000,000까지 허용합니다."));
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,
+                    "가격은 1,000원에서 1,000,000까지 허용합니다."));
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
-            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+//            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
+//            bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999까지 허용합니다."));
+            bindingResult.addError(
+                    new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999까지 허용합니다."));
         }
 
         //특정 필드가 아닌 복합룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
-                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값" + resultPrice);
+//                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값" + resultPrice);
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값" + resultPrice));
             }
         }
 
         //검증이 실패하면 다시 입력 뷰템플릿으로 다시 이동한다.
-        if(!errors.isEmpty()) {
-            log.info("error = {}", errors);
-            model.addAttribute("errors", errors);
+//        if(!errors.isEmpty()) {
+//            log.info("error = {}", errors);
+//            model.addAttribute("errors", errors);
+//            return "basic/addForm";
+//        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("error={}", bindingResult);
             return "basic/addForm";
         }
-
 
         //성공로직
         Item savedItem = itemRepository.save(item);
