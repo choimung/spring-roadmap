dc694a649be8e7fd89d7f452db3c3b7715185c52
choimung <103022503+choimung@users.noreply.github.com>
2024-01-08 13:25:46 +0900

    feat : [섹션 4] 오류 코드와 메세지 처리 완료

--- 김영한 MVC - 1편 하/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 김영한 MVC - 1편 하/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -18,6 +18,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -111,28 +112,44 @@ public class BasicItemController {
     @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes, Model model) {
+
+
+        /*
+        * `rejectValue()` 호출
+        * `MessageCodesResolvar`를 사용해서 검증 오류 코드로 메세지 코드들을 생성
+        * `new FieldError`를 생성하면서 메세지 코드를 보관한다.
+        * `th:errors`에서 메세지 코드들로 메세지를 순서대로 메세지에서 찾고 노출
+        * */
+
+        log.info("objectNMame={}", bindingResult.getObjectName());
+        log.info("target={}",bindingResult.getTarget());
+
         /*V1*/
         //검증 오류 보관
-//        Map<String, String> errors = new HashMap<>();
+        Map<String, String> errors = new HashMap<>();
 
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
 //            errors.put("itemName", "상품 이름은 필수입니다.");
 //            bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
-            bindingResult.addError(
-                    new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+//            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+//            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+            bindingResult.rejectValue("itemName", "required");
         }
+//            ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
+
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100000) {
 //            errors.put("price", "가격은 1,000원에서 1,000,000까지 허용합니다.");
 //            bindingResult.addError(new FieldError("item", "price", "가격은 1,000원에서 1,000,000까지 허용합니다."));
-            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,
-                    "가격은 1,000원에서 1,000,000까지 허용합니다."));
+//            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,
+//            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 100000},null));
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000},null);
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
 //            errors.put("quantity", "수량은 최대 9,999까지 허용합니다.");
 //            bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999까지 허용합니다."));
-            bindingResult.addError(
-                    new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999까지 허용합니다."));
+//            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
 
         //특정 필드가 아닌 복합룰 검증
@@ -140,7 +157,9 @@ public class BasicItemController {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
 //                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값" + resultPrice);
-                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값" + resultPrice));
+//                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값" + resultPrice));
+//                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"},new Object[]{10000},null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 
