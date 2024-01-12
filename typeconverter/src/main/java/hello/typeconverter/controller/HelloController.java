9f034d963d10ac64ee75df5d960c7e4e13776173
choimung <103022503+choimung@users.noreply.github.com>
2024-01-13 01:29:34 +0900

    [섹션 - 10] 스프링 타입 컨버터 - 스프링 Converter 적용하기

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,34 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV12(HttpServletRequest request) {
+        String data = request.getParameter("data"); // 문자 타입 으로 조회
+        Integer intValue = Integer.valueOf(data); //숫자 타입 으로 변환
+
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String helloV3(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
+        return "ok";
+    }
+
+}
